4a105c32432d4addee0d0daf25ca8583765f1db6
benjaAguilar <benjaaguilar02@gmail.com>
2023-11-25 13:24:36 -0300

    created add event listener on btnsort
    now when you click makes an array of all the entrys

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 let giveawaySection = document.querySelector("#giveaway-zone");
 let btnGiveaway = document.querySelector("#giveaway");
+let entrys = document.querySelector("#entrys");
+let btnSort = document.querySelector("#btn-s");
 
 giveawaySection.style.display = "none";
 
 btnGiveaway.addEventListener("click", ()=>{
     giveawaySection.style.display = "flex";
     btnGiveaway.classList.add("colored");
+});
+
+btnSort.addEventListener("click", ()=>{
+    let participants = entrys.value.split("\n");
+    console.log(participants);
 });
