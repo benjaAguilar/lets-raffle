47f451a21c637eddfcfaf56b17cf802d5692bad6
benjaAguilar <benjaaguilar02@gmail.com>
2023-11-28 12:27:00 -0300

    added a func to get randomNum to choose the winner

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ let btnSort = document.querySelector("#btn-s");
 
 giveawaySection.style.display = "none";
 
+function getRandomNum(val){
+    return Math.floor(Math.random() * val);
+}
+
 btnGiveaway.addEventListener("click", ()=>{
     giveawaySection.style.display = "flex";
     btnGiveaway.classList.add("colored");
@@ -12,5 +16,8 @@ btnGiveaway.addEventListener("click", ()=>{
 
 btnSort.addEventListener("click", ()=>{
     let participants = entrys.value.split("\n");
+    let winnerNum = getRandomNum(participants.length);
+
     console.log(participants);
+    console.log("the winner is " + participants[winnerNum]);
 });
